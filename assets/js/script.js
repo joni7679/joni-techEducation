411e20ca8933c6fb365c8be01253bd90f98132e1
joni <codingmylife0@gmail.com>
2024-09-01 23:31:02 +0530

    loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+window.addEventListener('load', function () {
+    // Hide the loader
+    document.querySelector('.loader').style.display = 'none';
+
+    // Show the main content
+    document.querySelector('main').style.display = 'block';
+});
+
 let toggleIocn = document.querySelector(".toggle-icon");
 let responsiveNavBar = document.querySelector(".responsive-navbar");
 toggleIocn.addEventListener("click", () => {
