856c0cb3d129fe29a528694ba6ad83c9233dfee9
joni <codingmylife0@gmail.com>
2024-09-04 00:09:31 +0530

    update all section and improve

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,46 @@
-window.addEventListener('load', function () {
-    // Hide the loader
-    document.querySelector('.loader').style.display = 'none';
 
-    // Show the main content
-    document.querySelector('main').style.display = 'block';
-});
-$(document).ready(function () {
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
+function loadAnimation() {
+    window.addEventListener('load', function () {
+        // Hide the loader
+        document.querySelector('.loader').style.display = 'none';
+
+        // Show the main content
+        document.querySelector('main').style.display = 'block';
+    });
+}
+loadAnimation()
+
+function counteranimation() {
+    $(document).ready(function () {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    });
+
+}
+
+counteranimation()
+
+function responsiveNavBar() {
+    let toggleIocn = document.querySelector(".toggle-icon");
+    let responsiveNavBar = document.querySelector(".responsive-navbar-mobile ");
+    toggleIocn.addEventListener("click", () => {
+        console.log("hello")
+        responsiveNavBar.classList.toggle("active-nav")
+    })
+}
+responsiveNavBar()
+
+
+// couse list box 
+let courseItems = document.querySelectorAll(".course-item");
+
+courseItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        // Remove 'active' class from all course items
+        courseItems.forEach(tab => tab.classList.remove('active'));        
+        // Add 'active' class to the clicked item
+        item.classList.add('active');
     });
 });
-
-let toggleIocn = document.querySelector(".toggle-icon");
-let responsiveNavBar = document.querySelector(".responsive-navbar");
-toggleIocn.addEventListener("click", () => {
-    console.log("hello")
-    responsiveNavBar.classList.toggle("active-nav")
-})
-
